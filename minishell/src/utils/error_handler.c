bd2d63fed8814da9964f5a5329b9a870d46c92ab
Nzola Kiampava <nzolakiampava@gmail.com>
2024-12-07 17:10:01 +0100

    Update error_handler.c

--- minishell/src/utils/error_handler.c
+++ minishell/src/utils/error_handler.c
@@ -12,83 +12,167 @@
 
 #include "minishell.h"
 
-void free_tokens(t_token *tokens) {
-    t_token *current;
-    
-    while (tokens) {
-        current = tokens;
+void print_error(char *msg)
+{
+    ft_putstr_fd("minishell: ", 2);
+    ft_putendl_fd(msg, 2);
+}
+
+void free_tokens(t_token *tokens)
+{
+    t_token *temp;
+
+    while (tokens)
+    {
+        temp = tokens;
         tokens = tokens->next;
-        
-        // Free the value of each token
-        if (current->value) {
-            free(current->value);
-        }
-        
-        // Free the token itself
-        free(current);
+        free(temp->value);
+        free(temp);
     }
 }
 
-void free_commands(t_command *commands) {
-    t_command *current;
+void free_commands(t_command *commands)
+{
+    t_command *temp;
     int i;
-    
-    while (commands) {
-        current = commands;
+
+    while (commands)
+    {
+        temp = commands;
         commands = commands->next;
         
-        // Free arguments
-        if (current->args) {
-            for (i = 0; current->args[i]; i++) {
-                free(current->args[i]);
-            }
-            free(current->args);
+        if (temp->args)
+        {
+            i = 0;
+            while (temp->args[i])
+                free(temp->args[i++]);
+            free(temp->args);
         }
         
-        // Free input and output file names
-        if (current->input_file) {
-            free(current->input_file);
-        }
-        if (current->output_file) {
-            free(current->output_file);
-        }
+        if (temp->input_file)
+            free(temp->input_file);
+        if (temp->output_file)
+            free(temp->output_file);
         
-        // Free the command structure
-        free(current);
+        free(temp);
     }
 }
 
-void free_shell(t_shell *shell) {
-    // Free environment variables
-    if (shell->env) {
-        for (int i = 0; shell->env[i]; i++) {
-            free(shell->env[i]);
+static char *get_temp_filename(void)
+{
+    static int heredoc_count = 0;
+    char *number_str;
+    char *filename;
+
+    number_str = ft_itoa(heredoc_count++);
+    if (!number_str)
+        return (NULL);
+    filename = ft_strjoin("/tmp/heredoc_", number_str);
+    free(number_str);
+    return (filename);
+}
+
+static int write_heredoc_content(int fd, char *delimiter)
+{
+    char *line;
+    int status;
+
+    status = EXIT_SUCCESS;
+    while (1)
+    {
+        line = readline("> ");
+        if (!line)
+        {
+            print_error("warning: here-document delimited by end-of-file");
+            break;
         }
+        if (ft_strcmp(line, delimiter) == 0)
+        {
+            free(line);
+            break;
+        }
+        if (write(fd, line, ft_strlen(line)) == -1 || 
+            write(fd, "\n", 1) == -1)
+        {
+            print_error("heredoc: write error");
+            status = EXIT_FAILURE;
+        }
+        free(line);
+        if (status == EXIT_FAILURE)
+            break;
+    }
+    return (status);
+}
+
+int handle_heredoc(t_command *cmd, char *delimiter)
+{
+    char *temp_file;
+    int fd;
+    int status;
+
+    temp_file = get_temp_filename();
+    if (!temp_file)
+        return (EXIT_FAILURE);
+
+    fd = open(temp_file, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    if (fd < 0)
+    {
+        free(temp_file);
+        return (EXIT_FAILURE);
+    }
+
+    status = write_heredoc_content(fd, delimiter);
+    close(fd);
+
+    if (status == EXIT_SUCCESS)
+    {
+        if (cmd->input_file)
+            free(cmd->input_file);
+        cmd->input_file = temp_file;
+    }
+    else
+    {
+        unlink(temp_file);
+        free(temp_file);
+        return (EXIT_FAILURE);
+    }
+
+    return (EXIT_SUCCESS);
+}
+
+static void cleanup_heredoc_files(void)
+{
+    char *temp_file;
+    int i;
+
+    i = 0;
+    while (i < 1000) // reasonable limit for cleanup
+    {
+        temp_file = get_temp_filename();
+        if (temp_file)
+        {
+            unlink(temp_file);
+            free(temp_file);
+        }
+        i++;
+    }
+}
+
+void free_shell(t_shell *shell)
+{
+    int i;
+
+    if (shell->env)
+    {
+        i = 0;
+        while (shell->env[i])
+            free(shell->env[i++]);
         free(shell->env);
     }
-    
-    // Free tokens
-    if (shell->tokens) {
-        free_tokens(shell->tokens);
-    }
-    
-    // Free commands
-    if (shell->commands) {
+    cleanup_heredoc_files();
+    if (shell->commands)
         free_commands(shell->commands);
-    }
     
-    // Reset shell structure
-    shell->env = NULL;
-    shell->tokens = NULL;
-    shell->commands = NULL;
-    shell->exit_status = 0;
-    shell->running = 0;
-}
-
-// Print error message to stderr
-void print_error(char *msg) {
-    if (msg) {
-        // Prefix the error message with the program name
-        fprintf(stderr, "minishell: %s\n", msg);
-    }
+    if (shell->tokens)
+        free_tokens(shell->tokens);
 }
