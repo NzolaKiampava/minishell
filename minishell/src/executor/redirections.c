5cdd92bf1cefc84f131eb54b083335a8cba91594
Nzola Kiampava <nzolakiampava@gmail.com>
2024-12-07 17:13:13 +0100

    Update redirections.c

--- minishell/src/executor/redirections.c
+++ minishell/src/executor/redirections.c
@@ -12,33 +12,23 @@
 
 #include "minishell.h"
 
-static int	handle_input_redirect(t_command *cmd)
+static int open_input_file(t_command *cmd)
 {
-    int	fd;
-
     if (cmd->input_file)
     {
-        fd = open(cmd->input_file, O_RDONLY);
-        if (fd < 0)
+        cmd->input_fd = open(cmd->input_file, O_RDONLY);
+        if (cmd->input_fd < 0)
         {
-            perror("open");
-            return (1);
+            print_error(strerror(errno));
+            return (EXIT_FAILURE);
         }
-        if (dup2(fd, STDIN_FILENO) < 0)
-        {
-            perror("dup2");
-            close(fd);
-            return (1);
-        }
-        close(fd);
     }
-    return (0);
+    return (EXIT_SUCCESS);
 }
 
-static int	handle_output_redirect(t_command *cmd)
+static int open_output_file(t_command *cmd)
 {
-    int	fd;
-    int	flags;
+    int flags;
 
     if (cmd->output_file)
     {
@@ -47,29 +37,75 @@ static int	handle_output_redirect(t_command *cmd)
             flags |= O_APPEND;
         else
             flags |= O_TRUNC;
-        
-        fd = open(cmd->output_file, flags, 0644);
-        if (fd < 0)
+        cmd->output_fd = open(cmd->output_file, flags, 0644);
+        if (cmd->output_fd < 0)
         {
-            perror("open");
-            return (1);
+            print_error(strerror(errno));
+            return (EXIT_FAILURE);
         }
-        if (dup2(fd, STDOUT_FILENO) < 0)
-        {
-            perror("dup2");
-            close(fd);
-            return (1);
-        }
-        close(fd);
     }
-    return (0);
+    return (EXIT_SUCCESS);
 }
 
-int	handle_redirections(t_command *cmd)
+static int setup_redirections(t_command *cmd)
 {
-    if (handle_input_redirect(cmd) != 0)
-        return (1);
-    if (handle_output_redirect(cmd) != 0)
-        return (1);
-    return (0);
+    if (cmd->input_fd != STDIN_FILENO)
+    {
+        if (dup2(cmd->input_fd, STDIN_FILENO) < 0)
+        {
+            print_error(strerror(errno));
+            return (EXIT_FAILURE);
+        }
+        close(cmd->input_fd);
+    }
+    if (cmd->output_fd != STDOUT_FILENO)
+    {
+        if (dup2(cmd->output_fd, STDOUT_FILENO) < 0)
+        {
+            print_error(strerror(errno));
+            return (EXIT_FAILURE);
+        }
+        close(cmd->output_fd);
+    }
+    return (EXIT_SUCCESS);
+}
+
+static void cleanup_redirections(t_command *cmd)
+{
+    if (cmd->input_fd != STDIN_FILENO)
+        close(cmd->input_fd);
+    if (cmd->output_fd != STDOUT_FILENO)
+        close(cmd->output_fd);
+}
+
+int handle_redirections(t_command *cmd)
+{
+    int status;
+
+    // Initialize file descriptors to standard input/output
+    cmd->input_fd = STDIN_FILENO;
+    cmd->output_fd = STDOUT_FILENO;
+
+    // Open input file if specified
+    if ((status = open_input_file(cmd)) != EXIT_SUCCESS)
+    {
+        cleanup_redirections(cmd);
+        return (status);
+    }
+
+    // Open output file if specified
+    if ((status = open_output_file(cmd)) != EXIT_SUCCESS)
+    {
+        cleanup_redirections(cmd);
+        return (status);
+    }
+
+    // Setup redirections (dup2)
+    if ((status = setup_redirections(cmd)) != EXIT_SUCCESS)
+    {
+        cleanup_redirections(cmd);
+        return (status);
+    }
+
+    return (EXIT_SUCCESS);
 }
