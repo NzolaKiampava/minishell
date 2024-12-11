a0c96d97ab1166e6d58c45a5a758cfd393427888
Nzola Kiampava <nzolakiampava@gmail.com>
2024-12-11 10:27:42 +0100

    Update executor.c

--- minishell/src/executor/executor.c
+++ minishell/src/executor/executor.c
@@ -12,110 +12,221 @@
 
 #include "minishell.h"
 
-char *find_command_path(char *cmd, char **env)
+// Função para encontrar o caminho completo do executável
+char *get_command_path(char *cmd, char **env)
 {
     char *path;
     char **paths;
     char *full_path;
-    char *temp;
     int i;
 
-    if (ft_strchr(cmd, '/'))
+    if (cmd[0] == '/' || cmd[0] == '.')
         return (ft_strdup(cmd));
-
     path = get_env_value(env, "PATH");
     if (!path)
         return (NULL);
-
     paths = ft_split(path, ':');
     if (!paths)
         return (NULL);
-
     i = 0;
     while (paths[i])
     {
-        temp = ft_strjoin(paths[i], "/");
-        full_path = ft_strjoin(temp, cmd);
-        free(temp);
-
-        if (access(full_path, X_OK) == 0)
+        full_path = ft_strjoin_three(paths[i], "/", cmd);
+        if (!access(full_path, X_OK))
         {
-            i = 0;
-            while (paths[i])
-                free(paths[i++]);
-            free(paths);
+            ft_free_array(paths);
             return (full_path);
         }
         free(full_path);
         i++;
     }
-
-    i = 0;
-    while (paths[i])
-        free(paths[i++]);
-    free(paths);
+    ft_free_array(paths);
     return (NULL);
 }
 
-static int execute_external_command(t_command *cmd, t_shell *shell)
+/*
+static void	close_pipe_fds(int *pipe_fds)
 {
-    char *cmd_path;
-    pid_t pid;
-    int status;
+    if (pipe_fds[0] != -1)
+        close(pipe_fds[0]);
+    if (pipe_fds[1] != -1)
+        close(pipe_fds[1]);
+}
+*/
 
-    if (!cmd->args || !cmd->args[0])
-        return (0);
-
-    cmd_path = find_command_path(cmd->args[0], shell->env);
-    if (!cmd_path)
+static int	setup_pipe(t_command *cmd, int *prev_pipe, int *curr_pipe)
+{
+    if (prev_pipe[0] != -1)
     {
-        print_error("command not found");
-        return (127);
+        if (dup2(prev_pipe[0], STDIN_FILENO) == -1)
+            return (0);
+        close(prev_pipe[0]);
+        prev_pipe[0] = -1;
     }
 
-    pid = fork();
-    if (pid == 0)
+    if (cmd->next)
     {
-        if (handle_redirections(cmd) != 0)
-            exit(1);
-        
-        execve(cmd_path, cmd->args, shell->env);
-        print_error("execution failed");
-        exit(1);
+        if (pipe(curr_pipe) == -1)
+            return (0);
+        if (dup2(curr_pipe[1], STDOUT_FILENO) == -1)
+            return (0);
+        close(curr_pipe[1]);
+        curr_pipe[1] = -1;
     }
-    else if (pid < 0)
-    {
-        print_error("fork failed");
-        free(cmd_path);
-        return (1);
-    }
-
-    free(cmd_path);
-    waitpid(pid, &status, 0);
-    
-    if (WIFEXITED(status))
-        return (WEXITSTATUS(status));
     return (1);
 }
 
-int execute_commands(t_shell *shell)
+static int	handle_heredoc(t_command *cmd)
 {
-    t_command *cmd;
-    int exit_status;
+    int		pipe_fd[2];
+    char	*line;
+    char	*delimiter;
+
+    if (pipe(pipe_fd) == -1)
+        return (-1);
+    delimiter = cmd->input_file;
+    while (1)
+    {
+        line = readline("> ");
+        if (!line || !ft_strcmp(line, delimiter))
+        {
+            free(line);
+            break;
+        }
+        write(pipe_fd[1], line, ft_strlen(line));
+        write(pipe_fd[1], "\n", 1);
+        free(line);
+    }
+    close(pipe_fd[1]);
+    return (pipe_fd[0]);
+}
+
+int	handle_redirections(t_command *cmd)
+{
+    if (cmd->input_file)
+    {
+        if (cmd->input_fd == -2)  // heredoc
+        {
+            cmd->input_fd = handle_heredoc(cmd);
+            if (cmd->input_fd == -1)
+                return (0);
+        }
+        else
+        {
+            cmd->input_fd = open(cmd->input_file, O_RDONLY);
+            if (cmd->input_fd == -1)
+            {
+                print_error("No such file or directory");
+                return (0);
+            }
+        }
+        if (dup2(cmd->input_fd, STDIN_FILENO) == -1)
+            return (0);
+        close(cmd->input_fd);
+    }
+
+    if (cmd->output_file)
+    {
+        cmd->output_fd = open(cmd->output_file, 
+            O_WRONLY | O_CREAT | (cmd->append_mode ? O_APPEND : O_TRUNC), 
+            0644);
+        if (cmd->output_fd == -1)
+        {
+            print_error("Cannot open output file");
+            return (0);
+        }
+        if (dup2(cmd->output_fd, STDOUT_FILENO) == -1)
+            return (0);
+        close(cmd->output_fd);
+    }
+    return (1);
+}
+
+static int	execute_single_command(t_command *cmd, t_shell *shell)
+{
+    pid_t	pid;
+    int		status;
+    char	*path;
+
+    if (is_builtin(cmd->args[0]))
+        return (execute_builtin(cmd, shell));
+
+    pid = fork();
+    if (pid == -1)
+        return (1);
+    if (pid == 0)
+    {
+        if (!handle_redirections(cmd))
+            exit(1);
+        path = get_command_path(cmd->args[0], shell->env);
+        if (!path)
+        {
+            print_error("Command not found");
+            exit(127);
+        }
+        execve(path, cmd->args, shell->env);
+        free(path);
+        exit(1);
+    }
+    waitpid(pid, &status, 0);
+    return (WEXITSTATUS(status));
+}
+
+static int	execute_piped_commands(t_command *cmd, t_shell *shell)
+{
+    pid_t	pid;
+    int		prev_pipe[2] = {-1, -1};
+    int		curr_pipe[2] = {-1, -1};
+    int		status;
+    int		last_status;
+
+    while (cmd)
+    {
+        pid = fork();
+        if (pid == -1)
+            return (1);
+        if (pid == 0)
+        {
+            if (!setup_pipe(cmd, prev_pipe, curr_pipe))
+                exit(1);
+            if (!handle_redirections(cmd))
+                exit(1);
+            if (is_builtin(cmd->args[0]))
+                exit(execute_builtin(cmd, shell));
+            execve(get_command_path(cmd->args[0], shell->env), 
+                cmd->args, shell->env);
+            exit(1);
+        }
+        if (prev_pipe[0] != -1)
+            close(prev_pipe[0]);
+        if (curr_pipe[1] != -1)
+            close(curr_pipe[1]);
+        prev_pipe[0] = curr_pipe[0];
+        prev_pipe[1] = curr_pipe[1];
+        cmd = cmd->next;
+    }
+    
+    while (wait(&status) > 0)
+        last_status = WEXITSTATUS(status);
+    return (last_status);
+}
+
+int	execute_commands(t_shell *shell)
+{
+    t_command	*cmd;
+    int			has_pipe;
 
     cmd = shell->commands;
     if (!cmd)
         return (0);
 
-    // Se houver mais de um comando, use pipes
-    if (cmd->next)
-        return (handle_pipes(cmd, shell));
+    // Check if we have any pipes
+    has_pipe = 0;
+    for (t_command *tmp = cmd; tmp->next; tmp = tmp->next)
+        has_pipe = 1;
 
-    // Se for um built-in, execute-o
-    if (is_builtin(cmd->args[0]))
-        return (execute_builtin(cmd, shell));
-
-    // Caso contrário, execute como comando externo
-    exit_status = execute_external_command(cmd, shell);
-    return (exit_status);
+    if (has_pipe)
+        return (execute_piped_commands(cmd, shell));
+    else
+        return (execute_single_command(cmd, shell));
 }
