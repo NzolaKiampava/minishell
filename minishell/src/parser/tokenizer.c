33bf5d6e47aebf170d6e58effb59e75d4f8e3ff3
Nzola Kiampava <nzolakiampava@gmail.com>
2024-12-20 05:39:54 -0500

    Update tokenizer.c

--- minishell/src/parser/tokenizer.c
+++ minishell/src/parser/tokenizer.c
@@ -1,82 +1,181 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nkiampav <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/11 14:09:39 by nkiampav          #+#    #+#             */
-/*   Updated: 2024/11/11 14:16:54 by nkiampav         ###   ########.fr       */
+/*   Created: 2024/12/06 10:17:49 by nkiampav          #+#    #+#             */
+/*   Updated: 2024/12/06 10:17:51 by nkiampav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int g_signal_received = 0;
-
-static void initialize_shell(t_shell *shell, char **env)
+static int is_space(char c)
 {
-    shell->env = init_env(env);
-    shell->commands = NULL;
-    shell->tokens = NULL;
-    shell->exit_status = 0;
-    shell->running = 1;
+    return (c == ' ' || c == '\t' || c == '\n');
 }
 
-static void process_input(t_shell *shell, char *input)
+static int is_operator(char c)
 {
-    if (!input)
-    {
-        // Handle Ctrl+D (EOF)
-        printf("exit\n");
-        shell->running = 0;
-        return;
-    }
-
-    if (strlen(input) > 0)
-    {
-        add_history(input);
-        shell->tokens = tokenize_input(input);
-        
-        if (shell->tokens)
-        {
-            shell->commands = parse_tokens(shell->tokens);
-            if (shell->commands)
-            {
-                expand_variables(shell->commands, shell);
-                shell->exit_status = execute_commands(shell);
-            }
-            free_commands(shell->commands);
-            shell->commands = NULL;
-        }
-        free_tokens(shell->tokens);
-        shell->tokens = NULL;
-    }
-    free(input);
+    return (c == '|' || c == '<' || c == '>');
 }
 
-int main(int argc, char **argv, char **env)
+static void add_token(t_token **head, char *value, t_token_type type)
 {
-    t_shell shell;
-    char *input;
+    t_token *new_token;
+    t_token *current;
 
-    (void)argc;
-    (void)argv;
+    new_token = (t_token *)malloc(sizeof(t_token));
+    if (!new_token)
+        return ;
+    new_token->value = value;
+    new_token->type = type;
+    new_token->next = NULL;
 
-    // Initialize shell
-    initialize_shell(&shell, env);
+    if (!*head)
+        *head = new_token;
+    else
+    {
+        current = *head;
+        while (current->next)
+            current = current->next;
+        current->next = new_token;
+    }
+}
+
+static int handle_quotes(char *input, int *i, char quote)
+{
+    int len;
+
+    len = 1;  // Start after the opening quote
+    while (input[*i + len] && input[*i + len] != quote)
+        len++;
     
-    // Setup signal handlers
-    setup_signals();
+    if (!input[*i + len])  // Quote was not closed
+        return (-1);
+    
+    return (len + 1);  // Include closing quote
+}
 
-    // Main shell loop
-    while (shell.running)
+static char *get_word(char *input, int *i)
+{
+    int start;
+    int len;
+    int quote_len;
+
+    start = *i;
+    len = 0;
+    while (input[*i + len])
     {
-        input = readline("minishell$ ");
-        process_input(&shell, input);
+        if (is_space(input[*i + len]) || is_operator(input[*i + len]))
+            break;
+        if (input[*i + len] == '\'' || input[*i + len] == '"')
+        {
+            quote_len = handle_quotes(input, i + len, input[*i + len]);
+            if (quote_len == -1)
+                return (NULL);  // Unclosed quote
+            len += quote_len;
+        }
+        else
+            len++;
     }
+    *i += len;
+    return (ft_substr(input, start, len));
+}
 
-    // Cleanup
-    free_shell(&shell);
-    return (shell.exit_status);
+static int get_operator_token(char *input, int *i, t_token **head)
+{
+    char *value;
+    t_token_type type;
+
+    if (input[*i] == '|')
+    {
+        if (input[*i + 1] == '|')
+            return (0);  // Error: || not supported
+        value = ft_strdup("|");
+        type = TOKEN_PIPE;
+        (*i)++;
+    }
+    else if (input[*i] == '<')
+    {
+        if (input[*i + 1] == '<')
+        {
+            if (input[*i + 2] == '<')
+                return (0);  // Error: <<< invalid
+            value = ft_strdup("<<");
+            type = TOKEN_HEREDOC;
+            (*i) += 2;
+        }
+        else
+        {
+            value = ft_strdup("<");
+            type = TOKEN_REDIRECT_IN;
+            (*i)++;
+        }
+    }
+    else if (input[*i] == '>')
+    {
+        if (input[*i + 1] == '>')
+        {
+            if (input[*i + 2] == '>')
+                return (0);  // Error: >>> invalid
+            value = ft_strdup(">>");
+            type = TOKEN_REDIRECT_APPEND;
+            (*i) += 2;
+        }
+        else
+        {
+            value = ft_strdup(">");
+            type = TOKEN_REDIRECT_OUT;
+            (*i)++;
+        }
+    }
+    else
+        return (0);
+
+    if (!value)
+        return (0);
+    add_token(head, value, type);
+    return (1);
+}
+
+t_token *tokenize_input(char *input)
+{
+    t_token *head;
+    int i;
+    char *word;
+
+    head = NULL;
+    i = 0;
+    while (input[i])
+    {
+        // Skip spaces
+        while (input[i] && is_space(input[i]))
+            i++;
+        if (!input[i])
+            break;
+
+        // Handle operators
+        if (is_operator(input[i]))
+        {
+            if (!get_operator_token(input, &i, &head))
+            {
+                free_tokens(head);
+                return (NULL);
+            }
+            continue;
+        }
+
+        // Handle words and quotes
+        word = get_word(input, &i);
+        if (!word)
+        {
+            free_tokens(head);
+            return (NULL);
+        }
+        add_token(&head, word, TOKEN_WORD);
+    }
+    return (head);
 }
