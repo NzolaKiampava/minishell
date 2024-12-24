73dc79480a5007eec4f5d582d3c467281c313bf1
Nzola Kiampava <nzolakiampava@gmail.com>
2024-12-24 11:56:06 -0500

    Update environment.c

--- minishell/src/utils/environment.c
+++ minishell/src/utils/environment.c
@@ -12,108 +12,108 @@
 
 #include "minishell.h"
 
-char **init_env(char **env)
+char	**init_env(char **env)
 {
-    char **new_env;
-    int i;
-    int size;
+	char	**new_env;
+	int	i;
+	int	size;
 
-    size = 0;
-    while (env[size])
-        size++;
+	size = 0;
+	while (env[size])
+		size++;
 
-    new_env = (char **)malloc(sizeof(char *) * (size + 1));
-    if (!new_env)
-        return (NULL);
+	new_env = (char **)malloc(sizeof(char *) * (size + 1));
+	if (!new_env)
+		return (NULL);
 
-    i = 0;
-    while (i < size)
-    {
-        new_env[i] = ft_strdup(env[i]);
-        if (!new_env[i])
-        {
-            while (--i >= 0)
-                free(new_env[i]);
-            free(new_env);
-            return (NULL);
-        }
-        i++;
-    }
-    new_env[i] = NULL;
-    return (new_env);
+	i = 0;
+	while (i < size)
+	{
+		new_env[i] = ft_strdup(env[i]);
+		if (!new_env[i])
+		{
+			while (--i >= 0)
+				free(new_env[i]);
+			free(new_env);
+			return (NULL);
+		}
+		i++;
+	}
+	new_env[i] = NULL;
+	return (new_env);
 }
 
-char *get_env_value(char **env, char *key)
+char	*get_env_value(char **env, char *key)
 {
-    int i;
-    int key_len;
+	int i;
+	int key_len;
 
-    if (!env || !key)
-        return (NULL);
+	if (!env || !key)
+		return (NULL);
 
-    key_len = ft_strlen(key);
-    i = 0;
-    while (env[i])
-    {
-        if (ft_strncmp(env[i], key, key_len) == 0 && env[i][key_len] == '=')
-            return (env[i] + key_len + 1);
-        i++;
-    }
-    return (NULL);
+	key_len = ft_strlen(key);
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], key, key_len) == 0 && env[i][key_len] == '=')
+		    return (env[i] + key_len + 1);
+		i++;
+	}
+	return (NULL);
 }
 
-int set_env_value(char ***env, char *key, char *value)
+int	set_env_value(char ***env, char *key, char *value)
 {
-    char *new_var;
-    char **new_env;
-    int i;
-    int size;
+	char *new_var;
+	char **new_env;
+	int i;
+	int size;
 
-    // Create new environment variable string
-    new_var = ft_strjoin(key, "=");
-    if (!new_var)
-        return (0);
-    new_var = ft_strjoin_free(new_var, value);
-    if (!new_var)
-        return (0);
+	// Create new environment variable string
+	new_var = ft_strjoin(key, "=");
+	if (!new_var)
+		return (0);
+	new_var = ft_strjoin_free(new_var, value);
+	if (!new_var)
+		return (0);
 
-    // Count current environment size
-    size = 0;
-    while ((*env)[size])
-        size++;
+	// Count current environment size
+	size = 0;
+	while ((*env)[size])
+		size++;
 
-    // Allocate new environment array
-    new_env = (char **)malloc(sizeof(char *) * (size + 2));
-    if (!new_env)
-    {
-        free(new_var);
-        return (0);
-    }
+	// Allocate new environment array
+	new_env = (char **)malloc(sizeof(char *) * (size + 2));
+	if (!new_env)
+	{
+		free(new_var);
+		return (0);
+	}
 
-    // Copy existing variables and add new one
-    i = 0;
-    while ((*env)[i])
-    {
-        new_env[i] = ft_strdup((*env)[i]);
-        if (!new_env[i])
-        {
-            while (--i >= 0)
-                free(new_env[i]);
-            free(new_env);
-            free(new_var);
-            return (0);
-        }
-        i++;
-    }
-    new_env[i] = new_var;
-    new_env[i + 1] = NULL;
+	// Copy existing variables and add new one
+	i = 0;
+	while ((*env)[i])
+	{
+		new_env[i] = ft_strdup((*env)[i]);
+		if (!new_env[i])
+		{
+			while (--i >= 0)
+				free(new_env[i]);
+			free(new_env);
+			free(new_var);
+			return (0);
+		}
+		i++;
+	}
+	new_env[i] = new_var;
+	new_env[i + 1] = NULL;
 
-    // Free old environment and update pointer
-    i = 0;
-    while ((*env)[i])
-        free((*env)[i++]);
-    free(*env);
-    *env = new_env;
+	// Free old environment and update pointer
+	i = 0;
+	while ((*env)[i])
+		free((*env)[i++]);
+	free(*env);
+	*env = new_env;
 
-    return (1);
+	return (1);
 }
