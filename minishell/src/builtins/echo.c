9ad3f1c32d8abeb4530e0a1c3fdb3946cba6753d
Nzola Kiampava <nzolakiampava@gmail.com>
2024-12-24 11:49:10 -0500

    Update echo.c

--- minishell/src/builtins/echo.c
+++ minishell/src/builtins/echo.c
@@ -12,33 +12,31 @@
 
 #include "minishell.h"
 
-int ft_echo(char **args)
+int	ft_echo(char **args)
 {
-    int i = 1;
-    int no_newline = 0;
+	int i = 1;
+	int no_newline = 0;
 
-    // Verifica se a flag -n está presente
-    if (args[1] && ft_strncmp(args[1], "-n", 2) == 0)
-    {
-        no_newline = 1;
-        i++;
-    }
+	// Verifica se a flag -n está presente
+	if (args[1] && ft_strncmp(args[1], "-n", 2) == 0)
+	{
+		no_newline = 1;
+		i++;
+	}
 
-    // Imprime os argumentos
-    while (args[i])
-    {
-        ft_putstr_fd(args[i], STDOUT_FILENO);
-        
-        // Adiciona espaço entre argumentos, exceto no último
-        if (args[i + 1])
-            ft_putstr_fd(" ", STDOUT_FILENO);
-        
-        i++;
-    }
+	// Imprime os argumentos
+	while (args[i])
+	{
+		write(STDOUT_FILENO, args[i], ft_strlen(args[i]));
 
-    // Adiciona nova linha, a menos que -n seja especificado
-    if (!no_newline)
-        ft_putstr_fd("\n", STDOUT_FILENO);
+		// Adiciona espaço entre argumentos, exceto no último
+		if (args[i + 1])
+			write(STDOUT_FILENO, " ", 1);
+		i++;
+	}
 
-    return EXIT_SUCCESS;
+	// Adiciona nova linha, a menos que -n seja especificado
+	if (!no_newline)
+		write(STDOUT_FILENO, "\n", 1);
+	return EXIT_SUCCESS;
 }
