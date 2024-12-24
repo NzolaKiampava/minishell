a77012611363aa33d0035f1b49c61f006883a44b
Nzola Kiampava <nzolakiampava@gmail.com>
2024-12-24 11:52:16 -0500

    Update unset.c

--- minishell/src/builtins/unset.c
+++ minishell/src/builtins/unset.c
@@ -13,58 +13,58 @@
 #include "minishell.h"
 
 // Função auxiliar para remover uma variável de ambiente
-static char **remove_env_var(char **env, int index)
+static char	**remove_env_var(char **env, int index)
 {
-    int env_len = 0;
-    char **new_env;
-    int j = 0;
+	int	env_len = 0;
+	char	**new_env;
+	int	j = 0;
 
-    // Conta o número de variáveis de ambiente
-    while (env[env_len])
-        env_len++;
+	// Conta o número de variáveis de ambiente
+	while (env[env_len])
+		env_len++;
 
-    // Aloca novo ambiente sem a variável a ser removida
-    new_env = malloc(sizeof(char *) * env_len);
-    if (!new_env)
-        return NULL;
+	// Aloca novo ambiente sem a variável a ser removida
+	new_env = malloc(sizeof(char *) * env_len);
+	if (!new_env)
+		return NULL;
 
-    // Copia variáveis, exceto a que será removida
-    for (int i = 0; i < env_len; i++)
-    {
-        if (i != index)
-            new_env[j++] = ft_strdup(env[i]);
-    }
-    new_env[j] = NULL;
+	// Copia variáveis, exceto a que será removida
+	for (int i = 0; i < env_len; i++)
+	{
+		if (i != index)
+			new_env[j++] = ft_strdup(env[i]);
+	}
+	new_env[j] = NULL;
 
-    // Libera o ambiente antigo
-    for (int i = 0; env[i]; i++)
-        free(env[i]);
-    free(env);
+	// Libera o ambiente antigo
+	for (int i = 0; env[i]; i++)
+		free(env[i]);
+	free(env);
 
-    return new_env;
+	return new_env;
 }
 
-int ft_unset(char **args, t_shell *shell)
+int	ft_unset(char **args, t_shell *shell)
 {
-    int i = 1;
+	int i = 1;
 
-    if (!args[1])
-        return EXIT_SUCCESS;
+	if (!args[1])
+		return EXIT_SUCCESS;
 
-    while (args[i])
-    {
-        for (int j = 0; shell->env[j]; j++)
-        {
-            // Verifica se o nome da variável corresponde
-            if (ft_strncmp(shell->env[j], args[i], ft_strlen(args[i])) == 0 &&
-                shell->env[j][ft_strlen(args[i])] == '=')
-            {
-                shell->env = remove_env_var(shell->env, j);
-                break;
-            }
-        }
-        i++;
-    }
+	while (args[i])
+	{
+		for (int j = 0; shell->env[j]; j++)
+		{
+			// Verifica se o nome da variável corresponde
+			if (ft_strncmp(shell->env[j], args[i], ft_strlen(args[i])) == 0 &&
+			shell->env[j][ft_strlen(args[i])] == '=')
+			{
+				shell->env = remove_env_var(shell->env, j);
+				break;
+			}
+		}
+		i++;
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
