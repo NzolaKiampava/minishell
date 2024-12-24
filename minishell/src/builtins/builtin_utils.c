ae970e58bc29bd3b968ac4a1d473995cd91f47e4
Nzola Kiampava <nzolakiampava@gmail.com>
2024-12-24 11:47:34 -0500

    Update builtin_utils.c

--- minishell/src/builtins/builtin_utils.c
+++ minishell/src/builtins/builtin_utils.c
@@ -13,49 +13,49 @@
 #include "minishell.h"
 
 // Função para verificar se o comando é um built-in
-int is_builtin(char *cmd)
+int	is_builtin(char *cmd)
 {
-    char *builtin_commands[] = {
-        "echo", 
-        "cd", 
-        "pwd", 
-        "export", 
-        "unset", 
-        "env", 
-        "exit",
-        NULL
-    };
+	char *builtin_commands[] = {
+		"echo", 
+		"cd", 
+		"pwd", 
+		"export", 
+		"unset", 
+		"env", 
+		"exit",
+		NULL
+	};
 
-    if (!cmd)
-        return 0;
+	if (!cmd)
+		return 0;
 
-    for (int i = 0; builtin_commands[i]; i++)
-    {
-        if (ft_strcmp(cmd, builtin_commands[i]) == 0)
-            return 1;
-    }
+	for (int i = 0; builtin_commands[i]; i++)
+	{
+		if (ft_strcmp(cmd, builtin_commands[i]) == 0)
+		    return 1;
+	}
 
-    return 0;
+	return 0;
 }
 
 // Função para executar o comando built-in correto
-int execute_builtin(t_command *cmd, t_shell *shell)
+int	execute_builtin(t_command *cmd, t_shell *shell)
 {
-    if (!cmd->args[0])
-        return EXIT_FAILURE;
-    if (ft_strcmp(cmd->args[0], "echo") == 0)
-        return (ft_echo(cmd->args));
-    else if (ft_strcmp(cmd->args[0], "cd") == 0)
-        return ft_cd(cmd->args, shell);
-    else if (ft_strcmp(cmd->args[0], "pwd") == 0)
-        return ft_pwd();
-    else if (ft_strcmp(cmd->args[0], "export") == 0)
-        return ft_export(cmd->args, shell);
-    else if (ft_strcmp(cmd->args[0], "unset") == 0)
-        return ft_unset(cmd->args, shell);
-    else if (ft_strcmp(cmd->args[0], "env") == 0)
-        return ft_env(shell);
-    else if (ft_strcmp(cmd->args[0], "exit") == 0)
-        return ft_exit(cmd->args, shell);
-    return EXIT_FAILURE;
+	if (!cmd->args[0])
+		return EXIT_FAILURE;
+	if (ft_strcmp(cmd->args[0], "echo") == 0)
+		return (ft_echo(cmd->args));
+	else if (ft_strcmp(cmd->args[0], "cd") == 0)
+		return ft_cd(cmd->args, shell);
+	else if (ft_strcmp(cmd->args[0], "pwd") == 0)
+		return ft_pwd();
+	else if (ft_strcmp(cmd->args[0], "export") == 0)
+		return ft_export(cmd->args, shell);
+	else if (ft_strcmp(cmd->args[0], "unset") == 0)
+		return ft_unset(cmd->args, shell);
+	else if (ft_strcmp(cmd->args[0], "env") == 0)
+		return ft_env(shell);
+	else if (ft_strcmp(cmd->args[0], "exit") == 0)
+		return ft_exit(cmd->args, shell);
+	return EXIT_FAILURE;
 }
