3cf4941a2cefefd1af9fa957ab6ba6a6cc6da10a
Manuel de Almeida <manuelalmeida70@outlook.pt>
2024-12-27 21:43:51 +0100

    Update: echo.c without norminette error(everything)

--- minishell/src/builtins/echo.c
+++ minishell/src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: nkiampav <nkiampav@student.42.fr>          #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024-12-06 20:07:32 by nkiampav          #+#    #+#             */
-/*   Updated: 2024-12-06 20:07:32 by nkiampav         ###   ########.fr       */
+/*   Updated: 2024/12/27 21:42:51 by mazunga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,24 @@
 
 int	ft_echo(char **args)
 {
-	int i = 1;
-	int no_newline = 0;
+	int	i;
+	int	no_newline;
 
-	// Verifica se a flag -n está presente
+	i = 1;
+	no_newline = 0;
 	if (args[1] && ft_strncmp(args[1], "-n", 2) == 0)
 	{
 		no_newline = 1;
 		i++;
 	}
-
-	// Imprime os argumentos
 	while (args[i])
 	{
 		write(STDOUT_FILENO, args[i], ft_strlen(args[i]));
-
-		// Adiciona espaço entre argumentos, exceto no último
 		if (args[i + 1])
 			write(STDOUT_FILENO, " ", 1);
 		i++;
 	}
-
-	// Adiciona nova linha, a menos que -n seja especificado
 	if (!no_newline)
 		write(STDOUT_FILENO, "\n", 1);
-	return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
