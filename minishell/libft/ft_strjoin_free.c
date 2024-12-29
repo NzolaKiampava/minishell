445d7996440a9191b1997db28777cb2f9d2e9403
Nzola Kiampava <nzolakiampava@gmail.com>
2024-12-29 11:36:41 -0500

    Update ft_strjoin_free.c

--- minishell/libft/ft_strjoin_free.c
+++ minishell/libft/ft_strjoin_free.c
@@ -14,29 +14,29 @@
 
 char	*ft_strjoin_free(char *new_var, char *value)
 {
-    char	*temp;
-    int		i;
-    int		j;
-    int		len1;
-    int		len2;
+	char	*temp;
+	int		i;
+	int		j;
+	int		len1;
+	int		len2;
 
-    if (!new_var)
-    {
-        temp = ft_strdup(value);
-        return (temp);
-    }
-    len1 = ft_strlen(new_var);
-    len2 = ft_strlen(value);
-    temp = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (!temp)
-        return (NULL);
-    i = -1;
-    while (new_var[++i])
-        temp[i] = new_var[i];
-    j = -1;
-    while (value[++j])
-        temp[i + j] = value[j];
-    temp[i + j] = '\0';
-    free(new_var);
-    return (temp);
+	if (!new_var)
+	{
+		temp = ft_strdup(value);
+		return (temp);
+	}
+	len1 = ft_strlen(new_var);
+	len2 = ft_strlen(value);
+	temp = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!temp)
+		return (NULL);
+	i = -1;
+	while (new_var[++i])
+		temp[i] = new_var[i];
+	j = -1;
+	while (value[++j])
+		temp[i + j] = value[j];
+	temp[i + j] = '\0';
+	free(new_var);
+	return (temp);
 }
