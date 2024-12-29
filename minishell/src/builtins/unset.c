bd98c4eb3ebbcb64c3c916002b59d65714683f59
Nzola Kiampava <nzolakiampava@gmail.com>
2024-12-29 11:23:36 -0500

    Update unset.c - norms errors solved

--- minishell/src/builtins/unset.c
+++ minishell/src/builtins/unset.c
@@ -12,59 +12,67 @@
 
 #include "minishell.h"
 
-// Função auxiliar para remover uma variável de ambiente
+static void	free_env_array(char **env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+	{
+		free(env[i]);
+		i++;
+	}
+	free(env);
+}
+
 static char	**remove_env_var(char **env, int index)
 {
-	int	env_len = 0;
-	char	**new_env;
-	int	j = 0;
+	char		**new_env;
+	int			env_len;
+	int			j;
+	int			i;
 
-	// Conta o número de variáveis de ambiente
+	env_len = 0;
 	while (env[env_len])
 		env_len++;
-
-	// Aloca novo ambiente sem a variável a ser removida
 	new_env = malloc(sizeof(char *) * env_len);
 	if (!new_env)
-		return NULL;
-
-	// Copia variáveis, exceto a que será removida
-	for (int i = 0; i < env_len; i++)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < env_len)
 	{
 		if (i != index)
 			new_env[j++] = ft_strdup(env[i]);
+		i++;
 	}
 	new_env[j] = NULL;
-
-	// Libera o ambiente antigo
-	for (int i = 0; env[i]; i++)
-		free(env[i]);
-	free(env);
-
-	return new_env;
+	free_env_array(env);
+	return (new_env);
 }
 
 int	ft_unset(char **args, t_shell *shell)
 {
-	int i = 1;
+	int	i;
+	int	j;
 
+	i = 1;
 	if (!args[1])
-		return EXIT_SUCCESS;
-
+		return (EXIT_SUCCESS);
 	while (args[i])
 	{
-		for (int j = 0; shell->env[j]; j++)
+		j = 0;
+		while (shell->env[j])
 		{
-			// Verifica se o nome da variável corresponde
-			if (ft_strncmp(shell->env[j], args[i], ft_strlen(args[i])) == 0 &&
-			shell->env[j][ft_strlen(args[i])] == '=')
+			if (ft_strncmp(shell->env[j], args[i], ft_strlen(args[i])) == 0
+				&& shell->env[j][ft_strlen(args[i])] == '=')
 			{
 				shell->env = remove_env_var(shell->env, j);
-				break;
+				break ;
 			}
+			j++;
 		}
 		i++;
 	}
-
-	return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
