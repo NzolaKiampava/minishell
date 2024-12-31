84df25f1f09555998087486d715e1182c3591857
Nzola Kiampava <nzolakiampava@gmail.com>
2024-12-31 15:46:58 -0500

    Update environment.c - solved lines codes numbers norminette error

--- minishell/src/utils/environment.c
+++ minishell/src/utils/environment.c
@@ -15,17 +15,15 @@
 char	**init_env(char **env)
 {
 	char	**new_env;
-	int	i;
-	int	size;
+	int		i;
+	int		size;
 
 	size = 0;
 	while (env[size])
 		size++;
-
 	new_env = (char **)malloc(sizeof(char *) * (size + 1));
 	if (!new_env)
 		return (NULL);
-
 	i = 0;
 	while (i < size)
 	{
@@ -45,8 +43,8 @@ char	**init_env(char **env)
 
 char	*get_env_value(char **env, char *key)
 {
-	int i;
-	int key_len;
+	int	i;
+	int	key_len;
 
 	if (!env || !key)
 		return (NULL);
@@ -56,64 +54,73 @@ char	*get_env_value(char **env, char *key)
 	while (env[i])
 	{
 		if (ft_strncmp(env[i], key, key_len) == 0 && env[i][key_len] == '=')
-		    return (env[i] + key_len + 1);
+			return (env[i] + key_len + 1);
 		i++;
 	}
 	return (NULL);
 }
 
-int	set_env_value(char ***env, char *key, char *value)
+static char	*create_env_var(const char *key, char *value)
 {
-	char *new_var;
-	char **new_env;
-	int i;
-	int size;
+	char	*new_var;
+	char	*full_var;
 
-	// Create new environment variable string
 	new_var = ft_strjoin(key, "=");
 	if (!new_var)
-		return (0);
-	new_var = ft_strjoin_free(new_var, value);
-	if (!new_var)
-		return (0);
+		return (NULL);
+	full_var = ft_strjoin_free(new_var, value);
+	return (full_var);
+}
 
-	// Count current environment size
-	size = 0;
-	while ((*env)[size])
-		size++;
+static char	**allocate_new_env(char **env, int size, char *new_var)
+{
+	char		**new_env;
+	int			i;
 
-	// Allocate new environment array
 	new_env = (char **)malloc(sizeof(char *) * (size + 2));
 	if (!new_env)
-	{
-		free(new_var);
-		return (0);
-	}
-
-	// Copy existing variables and add new one
+		return (NULL);
 	i = 0;
-	while ((*env)[i])
+	while (i < size)
 	{
-		new_env[i] = ft_strdup((*env)[i]);
+		new_env[i] = ft_strdup(env[i]);
 		if (!new_env[i])
 		{
 			while (--i >= 0)
 				free(new_env[i]);
 			free(new_env);
-			free(new_var);
-			return (0);
+			return (NULL);
 		}
 		i++;
 	}
-	new_env[i] = new_var;
-	new_env[i + 1] = NULL;
+	new_env[size] = new_var;
+	new_env[size + 1] = NULL;
+	return (new_env);
+}
 
-	// Free old environment and update pointer
-	i = 0;
-	while ((*env)[i])
-		free((*env)[i++]);
+int	set_env_value(char ***env, char *key, char *value)
+{
+	char		*new_var;
+	char		**new_env;
+	int			size;
+	int			i;
+
+	new_var = create_env_var(key, value);
+	if (!new_var)
+		return (0);
+	size = 0;
+	while ((*env)[size])
+		size++;
+	new_env = allocate_new_env(*env, size, new_var);
+	if (!new_env)
+	{
+		free(new_var);
+		return (0);
+	}
+	i = -1;
+	while ((*env)[++i])
+		free((*env)[i]);
 	free(*env);
 	*env = new_env;
-
 	return (1);
 }
