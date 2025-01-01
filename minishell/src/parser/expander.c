3e344dacf1aa1acbe587378aa53dd95d8907a0be
Nzola Kiampava <nzolakiampava@gmail.com>
2025-01-01 15:34:16 -0500

    Update expander.c - bug solved "echo $?", "echo $? + $?", "echo $"

--- minishell/src/parser/expander.c
+++ minishell/src/parser/expander.c
@@ -12,89 +12,76 @@
 
 #include "minishell.h"
 
-static char	*strip_quotes(char *str)
+static char	*handle_variable(char *str, int *i, t_shell *shell, char *result)
 {
-	int			len;
-	int			i;
-	int			j;
-	int			limit;
-	char		*result;
+	char	*var;
+	char	*temp;
 
-	len = ft_strlen(str);
-	result = malloc(len + 1);
-	if (!result)
-		return (NULL);
-	if (is_quoted(str, len))
-		i = 1;
-	else
-		i = 0;
-	j = 0;
-	limit = len;
-	if (is_quoted(str, len))
-		limit = len - 1;
-	while (str[i] && i < limit)
-		result[j++] = str[i++];
-	result[j] = '\0';
-	return (result);
+	var = expand_single_var(str, i, shell);
+	temp = ft_strjoin(result, var);
+	free(result);
+	free(var);
+	return (temp);
 }
 
 static char	*expand_string(char *str, t_shell *shell)
 {
-	int			i;
-	int			quotes[2];
-	char		*result;
-	char		*final_result;
-	char		*temp;
+	int		i;
+	char	*result;
+	int		quotes[2];
+	char	*final_result;
 
 	result = ft_strdup("");
-	i = -1;
+	i = 0;
 	quotes[0] = 0;
 	quotes[1] = 0;
-	while (str[++i])
+	while (str[i])
 	{
-		process_quotes(str[i], quotes);
+		update_quotes(str[i], quotes);
 		if (str[i] == '$' && !quotes[0])
-			result = process_expansion(str, &i, result, shell);
+			result = handle_variable(str, &i, shell, result);
 		else
-		{
-			temp = ft_charjoin(result, str[i]);
-			free(result);
-			result = temp;
-		}
+			result = ft_append_char(result, str[i++]);
 	}
 	final_result = strip_quotes(result);
 	free(result);
 	return (final_result);
 }
 
-static char	*expand_and_replace(char *original, t_shell *shell)
+static void	expand_one_file(char **file, t_shell *shell)
 {
 	char	*expanded;
 
-	if (!original)
-		return (NULL);
-	expanded = expand_string(original, shell);
-	free(original);
-	return (expanded);
+	if (!*file)
+		return ;
+	expanded = expand_string(*file, shell);
+	free(*file);
+	*file = expanded;
+}
+
+static void	expand_command_args(char **args, t_shell *shell)
+{
+	int		i;
+	char	*expanded;
+
+	i = 0;
+	while (args[i])
+	{
+		expanded = expand_string(args[i], shell);
+		free(args[i]);
+		args[i] = expanded;
+		i++;
+	}
 }
 
 void	expand_variables(t_command *cmd, t_shell *shell)
 {
-	int	i;
-
 	while (cmd)
 	{
 		if (cmd->args)
-		{
-			i = 0;
-			while (cmd->args[i])
-			{
-				cmd->args[i] = expand_and_replace(cmd->args[i], shell);
-				i++;
-			}
-		}
-		cmd->input_file = expand_and_replace(cmd->input_file, shell);
-		cmd->output_file = expand_and_replace(cmd->output_file, shell);
+			expand_command_args(cmd->args, shell);
+		expand_one_file(&cmd->input_file, shell);
+		expand_one_file(&cmd->output_file, shell);
 		cmd = cmd->next;
 	}
 }
